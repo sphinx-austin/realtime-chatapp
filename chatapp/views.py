58ca48447450d3f5cde30c36a684908cd614b3f0
sphinx-austin <austinjuniour4@gmail.com>
2023-07-24 13:11:56 +0300

    new views

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def test(request):
+    name = f"Hello"
+
+    context = {
+        name : 'Austine',
+
+
+    }
+
+    return render(request, 'chatapp/index.html', context)
